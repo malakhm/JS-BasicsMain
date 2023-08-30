5918ba3fa431ccadcc2ac5648b3ea2e26a9af3a8
Malak <malakhmm22@gmail.com>
2023-08-30 10:32:32 +0300

    Update for Step_2

--- javascript-basics-01/Step_2.js
+++ javascript-basics-01/Step_2.js
@@ -2,5 +2,5 @@ let name1 = 'Malak';
 let surname = 'Hamwi';
 let city = 'Beirut';
 
-alert("Hello "+ name1 + "\n" + "Surname: " + surname + "\n"+ "City: " + city);
+alert("Surname "+ surname + "\n" + "Name: " + name1 + "\n"+ "City: " + city);
 
