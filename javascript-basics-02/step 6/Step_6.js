755a7f11db5afaffb49fad4f03ab0689031e13b8
Malak <malakhmm22@gmail.com>
2023-08-30 16:43:12 +0300

    Completed Step_6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,46 @@
+// change image
+function changeImg(){
+    let imageOne;
+    let imageTwo;
+    let imageThree;
+    let imageFour;
+    let imageFive;
+
+    imageOne = document.getElementById('image1');
+    imageTwo = document.getElementById('image2');
+    imageThree = document.getElementById('image3');
+    imageFour = document.getElementById('image4');
+    imageFive = document.getElementById('image5');
+
+    imageOne.src = 'images/image1_2.jpg';
+    imageTwo.src = 'images/image2_2.jpg';
+    imageThree.src = 'images/image3_2.jpg';
+    imageFour.src = 'images/image4_2.jpg';
+    imageFive.src = 'images/image5_2.jpg';
+
+
+}
+
+
+// Return to the old img
+
+function changeImgBack(){
+    let imageOne;
+    let imageTwo;
+    let imageThree;
+    let imageFour;
+    let imageFive;
+
+    imageOne = document.getElementById('image1');
+    imageTwo = document.getElementById('image2');
+    imageThree = document.getElementById('image3');
+    imageFour = document.getElementById('image4');
+    imageFive = document.getElementById('image5');
+
+    imageOne.src = "images/image1.jpg" ;
+    imageTwo.src = "images/image2.jpg";
+    imageThree.src = "images/image3.jpg" ;
+    imageFour.src = "images/image4.jpg";
+    imageFive.src = "images/image5.jpg" ;
+   
+}
